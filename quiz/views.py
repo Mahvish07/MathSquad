54b1ec6f4977e5ab3041b1dcda5bc1348a31640d
@Abhijeet038 <gautamabhijeet038@gmail.com>
2025-04-16 15:16:09 +0530

    quiz

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here. 
+    
+def productDetails(request):
+
+    return render(request, "product_Details.html")
