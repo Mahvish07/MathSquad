497e89169577e887a564d7d02e171f936a383148
@Abhijeet038 <gautamabhijeet038@gmail.com>
2025-04-20 14:46:26 +0530

    ..

--- quiz/views.py
+++ quiz/views.py
@@ -2,6 +2,86 @@ from django.shortcuts import render
 
 # Create your views here. 
     
-def productDetails(request):
+def adminPanel(request):
 
-    return render(request, "product_Details.html")
+    return render(request, "admin panel.html")
+
+from django.shortcuts import render
+
+# Create your views here. 
+    
+def categoryPage(request):
+
+    return render(request, "category-page.html")
+
+from django.shortcuts import render
+
+# Create your views here. 
+    
+def communityDiscussion(request):
+
+    return render(request, "community & discussion.html")
+
+from django.shortcuts import render
+
+# Create your views here. 
+    
+def contest(request):
+
+    return render(request, "contest.html")
+
+from django.shortcuts import render
+
+# Create your views here. 
+    
+def index(request):
+
+    return render(request, "index.html")
+
+from django.shortcuts import render
+
+# Create your views here. 
+    
+def instancefeadback(request):
+
+    return render(request, "instancefeadback.html")
+
+from django.shortcuts import render
+
+# Create your views here. 
+    
+def notificationreminder(request):
+
+    return render(request, "notification-reminder.html")
+
+from django.shortcuts import render
+
+# Create your views here. 
+    
+def problemsolvingmodule(request):
+
+    return render(request, "problem-solvingModule.html")
+
+from django.shortcuts import render
+
+# Create your views here. 
+    
+def profilemanagment(request):
+
+    return render(request, "profile-management.html")
+
+from django.shortcuts import render
+
+# Create your views here. 
+    
+def rewardsachievement(request):
+
+    return render(request, "rewards-achievement.html")
+
+from django.shortcuts import render
+
+# Create your views here. 
+    
+def updateleaderboard(request):
+
+    return render(request, "update leaderboard.html")
