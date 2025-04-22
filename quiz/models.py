910fe88448b73a672abdec546b8d8d72a87b7401
Mahvish07 <fazamahvish@gmail.com>
2025-04-22 10:27:58 +0530

    dv

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Contest(models.Model):
+    name = models.CharField(max_length=255)  # Name of the contest
+    description = models.TextField(blank=True, null=True)  # Optional description
+    start_date = models.DateTimeField()  # Start date and time of the contest
+    end_date = models.DateTimeField()  # End date and time of the contest
+    is_active = models.BooleanField(default=True)  # Whether the contest is currently active
+
+    def __str__(self):
+        return self.name
